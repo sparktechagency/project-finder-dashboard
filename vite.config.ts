adf71ce5cb2a1087ef1fcc1902b6badbc62aada4
jewelrana3 <juyelrana7752@gmail.com>
2025-06-16 19:11:09 +0600

    asl;djfa

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: "147.93.94.210:6005",
+    host: "147.93.94.210",
   },
   resolve: {
     alias: {
