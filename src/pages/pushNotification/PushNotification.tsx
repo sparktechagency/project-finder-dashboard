e5ba21e6a8e07d038ad24a8a3907377da5120823
jewelrana3 <juyelrana7752@gmail.com>
2025-12-03 11:12:26 +0600

    done push notifications

--- src/pages/pushNotification/PushNotification.tsx
+++ src/pages/pushNotification/PushNotification.tsx
@@ -4,10 +4,12 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { useCreatePushNotificationMutation } from "@/redux/apiSlice/pushNofification/pushNotification";
+import { useRef } from "react";
 import toast from "react-hot-toast";
 
 export default function PushNotification() {
   const [pushNotification] = useCreatePushNotificationMutation();
+  const formRef = useRef<HTMLFormElement>(null);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -15,7 +17,6 @@ export default function PushNotification() {
     const formData = new FormData(e.currentTarget);
     const title = formData.get("title");
     const description = formData.get("description");
-    // const image = formData.get("image");
 
     const payload = {
       title,
@@ -24,12 +25,12 @@ export default function PushNotification() {
 
     try {
       const res = await pushNotification(payload).unwrap();
-      console.log("res", res);
+
       if (res.success) {
         toast.success(res.message || "Notification sent successfully", {
           id: "notification",
         });
-        // e.currentTarget.reset();
+        formRef.current?.reset();
       } else {
         toast.error(res.message || "Failed to send notification", {
           id: "notification",
@@ -47,7 +48,7 @@ export default function PushNotification() {
         Push Notification
       </h1>
       <div className="border border-gray-500 p-5 rounded mt-4">
-        <form className=" text-white" onSubmit={handleSubmit}>
+        <form ref={formRef} className=" text-white" onSubmit={handleSubmit}>
           <div className="mb-5">
             <Label
               htmlFor="title"
@@ -76,29 +77,6 @@ export default function PushNotification() {
               className="w-full px-4 py-2 h-32  border border-gray-400  placeholder-gray-400 text-black"
             />
           </div>
-          {/* image upload */}
-          {/* <div>
-            <Label
-              htmlFor="image"
-              className="block mb-2 text-sm font-medium text-gray-700"
-            >
-              Image
-            </Label>
-            <div className="border rounded p-4" onClick={handleClick}>
-              {imagePreview ? (
-                <img src={imagePreview} className="w-full h-[150px]" alt="" />
-              ) : (
-                <span className="text-gray-700">No Image</span>
-              )}
-            </div>
-            <Input
-              onChange={handleImageChange}
-              type="file"
-              id="image"
-              className="w-full px-4 h-11  border border-gray-400  placeholder-gray-400 hidden"
-              ref={inputRef}
-            />
-          </div> */}
 
           <Button
             className="w-full py-3 text-black cursor-pointer"
--- src/redux/apiSlice/pushNofification/pushNotification.ts
+++ src/redux/apiSlice/pushNofification/pushNotification.ts
@@ -4,7 +4,7 @@ const pushNotification = api.injectEndpoints({
   endpoints: (builder) => ({
     createPushNotification: builder.mutation({
       query: (data) => ({
-        url: "/notification/create",
+        url: "/push-notification/create",
         method: "POST",
         body: data,
       }),
